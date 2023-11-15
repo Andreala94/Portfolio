4f220e8b2f228fa3fa693396ecfad783562239b9
Andrea Lauro <125922974+Andreala94@users.noreply.github.com>
2023-11-15 19:56:11 +0100

    commit

--- src/Components/Competenze.jsx
+++ src/Components/Competenze.jsx
@@ -1,62 +1,77 @@
 import React from 'react'
-import  { Carousel }  from 'react-bootstrap'
-import CSS from '../Components/assets/css.png'
-import JS from '../Components/assets/js.jpg'
-import Node from '../Components/assets/node.jpg'
-import ReactJS from '../Components/assets/react.png'
+import Slider from 'react-infinite-logo-slider'
 
-function Competenze  () {
+import '../Components/Css/Competenze.css'
+import CSS from '../Components/assets/CSS.png'
+import JS from '../Components/assets/JS.png'
+import Node from '../Components/assets/Node.png'
+import ReactJS from '../Components/assets/React.png'
+import MongoDB from '../Components/assets/MongoDB.png'
+import Bootstrap from '../Components/assets/Bootstrap.png'
+import HTML from '../Components/assets/HTML.png'
+
+function Competenze() {
   return (
     <>
-    
-    <div>Competenze</div>
 
-    <div className='d-flex justify-content-center'>
-      
-    <Carousel>
-      <Carousel.Item>
-        <img
-          className="d-block w-25"
-          src={CSS}
-          alt="Prima immagine"
-        />
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-25"
-          src={JS}
-          alt="Seconda immagine"
-        />
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-25"
-          src={Node}
-          alt="Terza immagine"
-        />
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-25"
-          src={ReactJS}
-          alt="Quarta immagine"
-        />
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-25"
-          src="https://via.placeholder.com/800x400/ffff00/000000?text=5"
-          alt="Quinta immagine"
-        />
-      </Carousel.Item>
-    </Carousel>
-    </div>
-    
+      <div>Competenze</div>
+
+      <div className='mt-5 w-75 d-flex'>
+        <Slider
+          width="250px"
+          duration={30}
+          pauseOnHover={true}
+          blurBorders={true}
+          blurBoderColor={'#fff'}
+        >
+          <Slider.Slide>
+            <div className='text-center stack_icon_hover'>
+              <img src={Bootstrap} alt="any" className='w-30 stack_icon' />
+            </div>
+          </Slider.Slide>
+          <Slider.Slide>
+            <div className='text-center stack_icon_hover'>
+              <img src={CSS} alt="any" className='w-30 stack_icon' />
+            </div>
+          </Slider.Slide>
+          <Slider.Slide>
+            <div className='text-center stack_icon_hover'>
+              <img src={HTML} alt="any" className='w-30 stack_icon' />
+            </div>
+          </Slider.Slide>
+          <Slider.Slide>
+            <div className='text-center stack_icon_hover'>
+              <img src={JS} alt="any" className='w-30 stack_icon' />
+            </div>
+          </Slider.Slide>
+          <Slider.Slide>
+            <div className='text-center stack_icon_hover'>
+              <img src={MongoDB} alt="any" className='w-30 stack_icon' />
+            </div>
+          </Slider.Slide>
+          <Slider.Slide>
+            <div className='text-center stack_icon_hover'>
+              <img src={ReactJS} alt="any" className='w-30 stack_icon' />
+            </div>
+          </Slider.Slide>
+          <Slider.Slide>
+            <div className='text-center stack_icon_hover'>
+              <img src={Node} alt="any" className='w-30 stack_icon' />
+            </div>
+          </Slider.Slide>
+        </Slider>
+
+
+
+
+
+      </div>
+
     </>
 
 
 
-    
+
   )
 }
 
