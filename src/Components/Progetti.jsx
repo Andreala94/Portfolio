0fc16302e865a25fd4e2ebda5158a5195a794fc9
Andrea Lauro <125922974+Andreala94@users.noreply.github.com>
2023-12-11 22:26:15 +0100

    commit

--- src/Components/Progetti.jsx
+++ src/Components/Progetti.jsx
@@ -32,14 +32,14 @@ const Progetti = () => {
         <a href="https://github.com/Andreala94" target="_blank" rel="noopener noreferrer" onClick={openGitHub}>
           <Image src={ProfiloGit} className='immagine' fluid />
           </a>
-          <p>Profilo GhitHub</p>
+          <p className='mt-2'>Profilo GhitHub</p>
         </Col>
     
         <Col xs={12} md={6}>
         <a href="https://eventipuglia.netlify.app" target="_blank" rel="noopener noreferrer" onClick={openEventi}>
           <Image src={EventiPuglia}  className='immagine' fluid />
         </a>
-        <p>Sito Eventi in Puglia dove un utente può controllare che eventi ci sono in puglia,
+        <p className='mt-2'>Sito Eventi in Puglia dove un utente può controllare che eventi ci sono in puglia,
           ma può anche tramite la registrazione e il Login creare un proprio Evento nella propria città.
         </p>
         </Col>
@@ -48,7 +48,7 @@ const Progetti = () => {
           <a href="https://chipper-mochi-c8f6bd.netlify.app/" target='-blank' rel="noopener noreferrer" onClick={openNetflix}>
             <Image src={Netflix} className='immagine mt-3' fluid />
           </a>
-          <p>ksfjdljfslsdmkfdlsfklfk</p>
+          <p className='mt-2'>Interfaccia replica Netflix creata  in HTML e CSS</p>
         </Col>
       </Row>
     </Container>
